720aea8b6ef59472f838a9237e1f22e19c60cf5f
Craig Puxty <craig_puxty@talktalk.net>
2021-09-03 21:37:50 +0100

    NextAuth Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
+import { Provider } from "next-auth/client";
+
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </Provider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import { useSession } from "next-auth/client";
+
 function Home() {
+  const [session, loading] = useSession();
+
   return (
     <main className="main">
       <div className="container">
         <h1>Home Page</h1>
+        <p>Welcome to Next Auth {session ? session.user.name : "Guest"}</p>
       </div>
     </main>
   );
